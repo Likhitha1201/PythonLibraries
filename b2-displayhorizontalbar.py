4d946e653d0f88e7b7b427f987ba54548f5d667d
likhithashylu <likitha.shylu@gmail.com>
2024-10-21 23:19:00 +0530

    [LIKHITHA] add-Applying logic to create horizontal bar.

---
+++ b2-displayhorizontalbar.py
@@ -0,0 +1,31 @@
+""" 
+    @Author: Likhitha S
+    @Date: 18-10-2024 17:30
+    @Last Modified by: Likhitha S
+    @Last Modified time: 18-10-2024 17:30
+    @Title: write a Python programming to display a horizontal bar chart of the popularity of programming Languages.  
+
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    """
+
+        Description: 
+            This function is used to draw a bar graph for given plots.
+        Parameters: 
+            prg_lang, popularity are the list of points that as to be displayed on graph.
+        return:
+            It displays the bar graph.
+
+    """
+    
+    prg_lang=np.array(['java', 'python', 'PHP','javascript','c#','c++'])
+    popularity= np.array([22.2,17.6,8.8,8,7.7,6.7])
+    
+    plt.barh(prg_lang, popularity, color='lightgreen')
+    plt.show()
+   
+if __name__=="__main__":
+    main()
