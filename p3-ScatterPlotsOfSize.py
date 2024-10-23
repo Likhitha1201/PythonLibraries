a67b0d423329a93f423a9ffc459d6f75169b90c0
likhithashylu <likitha.shylu@gmail.com>
2024-10-23 09:28:51 +0530

    [LIKHITHA] add-Applying logic to draw scatter graph with random values and sizes.

---
+++ p3-ScatterPlotsOfSize.py
@@ -0,0 +1,38 @@
+""" 
+    @Author: Likhitha S
+    @Date: 23-10-2024 9:30
+    @Last Modified by: Likhitha S
+    @Last Modified time: 23-10-2024 9:30
+    @Title: Write a Python program to draw a scatter plot using random distributions to generate balls of 
+    different sizes.
+       
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    """
+
+        Description: 
+            This function is used to draw a scatter graph with different size of plots.
+        Parameters: 
+            x, y are the plots taken from the random.randint()
+        return:
+            It displays the graph with scatter plots.
+
+    """
+    
+    x=np.random.rand(1, 10, 50)
+    y=np.random.rand(1, 10, 50)
+    size=np.random.rand(1, 10, 50)
+    plt.scatter(x, y, marker='o', s=size, color='green', alpha=0.7)
+    plt.title("Scatter plot of random values", size=15, color='r')
+    plt.xlabel("x-values", size=12)
+    plt.ylabel("y-values", size=12)
+    
+    plt.show()
+   
+if __name__=="__main__":
+    main()
